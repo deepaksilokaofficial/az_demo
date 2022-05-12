db8f22432c84a717ce17863348e384cfedd42d77
deepaksilokaofficial <58412342+deepaksilokaofficial@users.noreply.github.com>
2022-05-12 11:45:06 +0530

    Update score.py

--- score.py
+++ score.py
@@ -1,21 +1,25 @@
 import os
+import logging
 import json
-import numpy 
-import joblib
+import numpy
+import pickle
 from azure.storage.blob import BlobServiceClient
 
-
 def init():
-    conn_str = "DefaultEndpointsProtocol=https;AccountName=mywsstorage07701d4b68654;AccountKey=XHmrGY5DZ4PmqAoPBdyaAWAeH/G+Ox+Az8sg/4Dr0TM1kjiZXiS51DAscIiuVwuCsQIa0lKAFOOI+AStaNR1KA==;EndpointSuffix=core.windows.net"
-    name = demo_blob
-    global blob_service_client = BlobServiceClient.from_connection_string(conn_str)    
+    
+    blob_connection_string = "DefaultEndpointsProtocol=https;AccountName=mywsstorage07701d4b68654;AccountKey=XHmrGY5DZ4PmqAoPBdyaAWAeH/G+Ox+Az8sg/4Dr0TM1kjiZXiS51DAscIiuVwuCsQIa0lKAFOOI+AStaNR1KA==;EndpointSuffix=core.windows.net"
+    blob_container_name = "blob-demo"
+
+    global blob_service_client
+    blob_service_client = BlobServiceClient.from_connection_string(blob_connection_string)
+    
     global model
-    model = joblib.load('model.pkl')
+    #model = joblib.load('model.pkl')
     
 
 def run(raw_data):
     
-    blob_client = blob_service_client.get_blob_client(container=name,blob=raw_data['model'])
+    blob_client = blob_service_client.get_blob_client(container=blob_container_name,blob=raw_data['model'])
     blob_data = blob_client.download_blob()
     aa = blob_data.readall()
     model = pickle.loads(aa)
